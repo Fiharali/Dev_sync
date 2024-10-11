f676cc7adf9554b5b20847b415be4286cf349c1c
ali <fiharali0@gmail.com>
2024-10-11 22:33:29 +0200

    add TaskSchedulerListener

---
+++ src/main/java/com/devsync/scheduled/TaskSchedulerListener.java
@@ -0,0 +1,29 @@
+package com.devsync.scheduled;
+
+import com.devsync.scheduled.TaskRequestUpdater;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.Timer;
+
+@WebListener
+public class TaskSchedulerListener implements ServletContextListener {
+
+    private Timer timer;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        timer = new Timer(true);
+        TaskRequestUpdater task = new TaskRequestUpdater();
+
+        timer.scheduleAtFixedRate(task, 0, 24*60*60 * 1000);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+}
